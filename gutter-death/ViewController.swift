ca80b8716f446b87364fe8475ed4398fc260d555
Noah <nwilde1590@gmail.com>
2017-11-05 16:06:54 -0500

    Initial Commit

---
+++ gutter-death/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gutter-death
+//
+//  Created by user908469 on 11/5/17.
+//  Copyright © 2017 nwilde1590. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ gutter-deathTests/gutter_deathTests.swift
@@ -0,0 +1,36 @@
+//
+//  gutter_deathTests.swift
+//  gutter-deathTests
+//
+//  Created by user908469 on 11/5/17.
+//  Copyright © 2017 nwilde1590. All rights reserved.
+//
+
+import XCTest
+@testable import gutter_death
+
+class gutter_deathTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
